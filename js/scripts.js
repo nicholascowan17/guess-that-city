4cd4bbefb41e3d0728b1d167f80facc83f7d2b31
nicholascowan17 <78500058+nicholascowan17@users.noreply.github.com>
2021-02-08 22:07:44 -0500

    JQuery function added to display right/wrong overlays

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,44 @@
 // get the button and make it respond to a click
 var theButton = document.getElementById("b");
-theButton.onclick = showResults;
+theButton.onclick = runBoth;
 
-// function Score( input ) {
-//   if(input==0){return "You did not recognize any cities. Better luck next time!"}
-//   else if(input <= 3 && input >= 1){return "You knew a few cities, but you could do better."}
-//   else if(input <= 6 && input >= 4){return "You knew several cities, but missed a handful. Not bad!"}
-//   else if(input <= 8 && input >= 7){return "You knew most of the cities. Great job!"}
-//   else if(input==9){return "Amazing! You got them all right!"}
-//   else{return "You did not enter a valid number."}
-//   }
-//
-// // this function runs each time the button is clicked
-// function feedTheButton() {
-//   // get the response from the text input field
-//   // parseInt() changes string to number
-//   var input = parseInt(document.getElementById("in").value);
-//   // run the function above this one and store what is returned in result
-//   var result = Score( input );
-//   // write the result into the HTML
-//   var place = document.getElementById("write");
-//   place.innerHTML = result;
-// }
+function runBoth() {
+  showResults();
+  showOverlays();
+}
 
 function showResults() {
-var total = 0;
-if ($('#atlanta').val().toLowerCase().trim() == 'atlanta'){total++}
-if ($('#boston').val().toLowerCase().trim() == 'boston'){total++}
-if ($('#dubai').val().toLowerCase().trim() == 'dubai'){total++}
-if ($('#irvine').val().toLowerCase().trim() == 'irvine'){total++}
-if (($('#nyc').val().toLowerCase().trim() == 'nyc') || ($('#nyc').val().toLowerCase().trim() == 'new york city') || ($('#nyc').val().toLowerCase().trim() == 'new york')){total++}
-if ($('#osaka').val().toLowerCase().trim() == 'osaka'){total++}
-if ($('#paris').val().toLowerCase().trim() == 'paris'){total++}
-if ($('#rome').val().toLowerCase().trim() == 'rome'){total++}
-if (($('#sf').val().toLowerCase().trim() == 'sf') || ($('#sf').val().toLowerCase().trim() == 'san francisco')){total++}
-var place = document.getElementById("write");
-place.innerHTML = total.toString().concat("/9");
+  var total = 0;
+  if ($('#atlanta').val().toLowerCase().trim() == 'atlanta'){total++}
+  if ($('#boston').val().toLowerCase().trim() == 'boston'){total++}
+  if ($('#dubai').val().toLowerCase().trim() == 'dubai'){total++}
+  if ($('#irvine').val().toLowerCase().trim() == 'irvine'){total++}
+  if (($('#nyc').val().toLowerCase().trim() == 'nyc') || ($('#nyc').val().toLowerCase().trim() == 'new york city') || ($('#nyc').val().toLowerCase().trim() == 'new york')){total++}
+  if ($('#osaka').val().toLowerCase().trim() == 'osaka'){total++}
+  if ($('#paris').val().toLowerCase().trim() == 'paris'){total++}
+  if ($('#rome').val().toLowerCase().trim() == 'rome'){total++}
+  if (($('#sf').val().toLowerCase().trim() == 'sf') || ($('#sf').val().toLowerCase().trim() == 'san francisco')){total++}
+  var place = document.getElementById("write");
+  place.innerHTML = total.toString().concat("/9");
+}
+
+function showOverlays() {
+  if ($('#atlanta').val().toLowerCase().trim() == 'atlanta'){$('#overlay1a')[0].style.visibility = 'visible';}
+    else {$('#overlay1b')[0].style.visibility = 'visible';}
+  if ($('#boston').val().toLowerCase().trim() == 'boston'){$('#overlay2a')[0].style.visibility = 'visible';}
+    else {$('#overlay2b')[0].style.visibility = 'visible';}
+  if ($('#dubai').val().toLowerCase().trim() == 'dubai'){$('#overlay3a')[0].style.visibility = 'visible';}
+    else {$('#overlay3b')[0].style.visibility = 'visible';}
+  if ($('#irvine').val().toLowerCase().trim() == 'irvine'){$('#overlay4a')[0].style.visibility = 'visible';}
+    else {$('#overlay4b')[0].style.visibility = 'visible';}
+  if (($('#nyc').val().toLowerCase().trim() == 'nyc') || ($('#nyc').val().toLowerCase().trim() == 'new york city') || ($('#nyc').val().toLowerCase().trim() == 'new york')){$('#overlay5a')[0].style.visibility = 'visible';}
+    else {$('#overlay5b')[0].style.visibility = 'visible';}
+  if ($('#osaka').val().toLowerCase().trim() == 'osaka'){$('#overlay6a')[0].style.visibility = 'visible';}
+    else {$('#overlay6b')[0].style.visibility = 'visible';}
+  if ($('#paris').val().toLowerCase().trim() == 'paris'){$('#overlay7a')[0].style.visibility = 'visible';}
+    else {$('#overlay7b')[0].style.visibility = 'visible';}
+  if ($('#rome').val().toLowerCase().trim() == 'rome'){$('#overlay8a')[0].style.visibility = 'visible';}
+    else {$('#overlay8b')[0].style.visibility = 'visible';}
+  if (($('#sf').val().toLowerCase().trim() == 'sf') || ($('#sf').val().toLowerCase().trim() == 'san francisco')){$('#overlay9a')[0].style.visibility = 'visible';}
+    else {$('#overlay9b')[0].style.visibility = 'visible';}
 }
